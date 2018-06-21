dee380c69c247130f57dcec9e3f6c1f1b24653a0
promavto <promavto@ntmp.ru>
2018-06-21 06:48:55 +0300

    с

--- USB_Host_SAMD21/Release/core/WMath.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\USB_Host_SAMD21\arduino_zero_native\Release\core\WMath.cpp.o: \
- C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\samd\1.6.18\cores\arduino\WMath.cpp \
- C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\samd\1.6.18\cores\arduino\WMath.h
